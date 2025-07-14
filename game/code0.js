4cd183cebd78e0721b1a600a44883eebfb2efbed
42saisiot <42saisiot42@gmail.com>
2025-07-15 06:40:38 +0900

    Add game

--- game/code0.js
+++ game/code0.js
@@ -1,20 +1,20 @@
-gdjs.Untitled_32sceneCode = {};
-gdjs.Untitled_32sceneCode.localVariables = [];
+gdjs.Card_32screenCode = {};
+gdjs.Card_32screenCode.localVariables = [];
 
 
-gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
+gdjs.Card_32screenCode.eventsList0 = function(runtimeScene) {
 
 };
 
-gdjs.Untitled_32sceneCode.func = function(runtimeScene) {
+gdjs.Card_32screenCode.func = function(runtimeScene) {
 runtimeScene.getOnceTriggers().startNewFrame();
 
 
-gdjs.Untitled_32sceneCode.eventsList0(runtimeScene);
+gdjs.Card_32screenCode.eventsList0(runtimeScene);
 
 
 return;
 
 }
 
-gdjs['Untitled_32sceneCode'] = gdjs.Untitled_32sceneCode;
+gdjs['Card_32screenCode'] = gdjs.Card_32screenCode;
